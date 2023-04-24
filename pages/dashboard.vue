46b85036da344f693e5e793ebec75667a1d9b29b
Retsuki <30931666+Retsuki@users.noreply.github.com>
2023-04-24 23:18:16 +0900

    Initial commit

---
+++ pages/dashboard.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Dashboard</h1>
+
+  <pre>{{ cookie }}</pre>
+</template>
+
+<script lang="ts" setup>
+console.log('Dashboard setup');
+const cookie = useCookie('cookie', { default: () => 1 });
+</script>
